b547d4667eabbcded567ceedbde75547193410d1
alexandelton <alexanderelton2887@gmail.com>
2023-01-20 02:14:38 -0500

    newsss

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -2,4 +2,4 @@ let text = document.lastModified;
 document.getElementById("lastModified").innerHTML = text;
 
 let time = "2023"
-document.getElementById("p").innerHTML = time;
+document.getElementById("date").innerHTML = time;
