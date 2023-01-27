fc9fdc789e1810c6a5ce59e268bed60a3aa6ecb9
Alexander Chagua <109567411+alexanderchagua@users.noreply.github.com>
2023-01-27 10:52:01 -0500

    newss

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -3,3 +3,15 @@ document.getElementById("lastModified").innerHTML = text;
 
 let time = "2023"
 document.getElementById("date").innerHTML = time;
+
+
+var d = new Date();
+
+    // Set the value of the "date" field
+    document.getElementById("date").value = d.toDateString();
+
+    // Set the value of the "time" field
+    var hours = d.getHours();
+    var mins = d.getMinutes();
+    var seconds = d.getSeconds();
+    document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
