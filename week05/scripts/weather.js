068c4644610f8562966c71c518989308eaa9b7d8
alexandelton <alexanderelton2887@gmail.com>
2023-02-02 00:41:41 -0500

    newws

---
+++ week05/scripts/weather.js
@@ -0,0 +1,35 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const mykey = "843c1c38f9dbad88c862b6e4af7e9a92";
+const nameCity = "Trier"
+const unit = "imperial"
+const url = 'http://api.openweathermap.org/data/2.5/weather?q=${nameCity}&appid=${mykey}&units=${unit}';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); 
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+  function DisplayResult(data){
+    currentTemp.innerHTML = "${data.main.temp}&deg;F";
+    const iconsrc = "https://openweathermap.org/img/w/${data.weather[0].icon}10d.png";
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute("src",iconsrc);
+    weatherIcon.setAttribute("alt",desc);
+
+  }
+
+  
