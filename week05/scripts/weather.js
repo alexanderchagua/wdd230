5fe92eacdc8406d13761b24473cc971fbf5babc4
Alexander Chagua <109567411+alexanderchagua@users.noreply.github.com>
2023-02-02 13:17:24 -0500

    Update weather.js

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -25,7 +25,7 @@ async function apiFetch() {
 
   function DisplayResult(data){
     currentTemp.innerHTML = "${data.main.temp}&deg;F";
-    const iconsrc = "https://openweathermap.org/img/w/${data.weather[0].icon}10d.png";
+    const iconsrc = "https://openweathermap.org/img/w/${data.weather[0].icon}04n.png";
     let desc = data.weather[0].description;
     weatherIcon.setAttribute("src",iconsrc);
     weatherIcon.setAttribute("alt",desc);
