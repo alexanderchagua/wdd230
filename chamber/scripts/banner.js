c8248a7ce836ae57ce8e946a509ed9bd352f4a9e
alexandelton <alexanderelton2887@gmail.com>
2023-02-13 01:56:17 -0500

    newsss

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -12,21 +12,21 @@ let  banner = document.querySelector("#banner")
 
 if(day == 1) {
     let t = document.createElement("h3")
-    t.classList.add('sectioncard');
-    t.innerHTML = "Greetings to all, we invite you to join our meeting of the Lima Chamber of Commerce on Wednesday at 7:00 p.m.";
+    t.classList.add('bannerwed');
+    t.innerHTML = "\n \n Greetings to all, we invite you to join our meeting of the Lima Chamber of Commerce on Wednesday at 7:00 p.m.";
     banner.appendChild(t)
     
 }
 else if(day == 2){
     let t = document.createElement("h3")
-    t.classList.add('sectioncard');
-    t.innerHTML = "Greetings to all, we invite you to join our meeting of the Lima Chamber of Commerce on Wednesday at 7:00 p.m.";
+    t.classList.add('bannerwed');
+    t.innerHTML = "\n Greetings to all, we invite you to join our meeting of the Lima Chamber of Commerce on Wednesday at 7:00 p.m.";
     banner.appendChild(t)
 }
 else if(day == 3) {
     let t = document.createElement("h3")
-    t.classList.add('sectioncard');
-    t.innerHTML = "Greetings to all, we invite you to join our meeting of the Lima Chamber of Commerce on Wednesday at 7:00 p.m.";
+    t.classList.add('bannerwed');
+    t.innerHTML = "\n \n Greetings to all, we invite you to join our meeting of the Lima Chamber of Commerce on Wednesday at 7:00 p.m.";
     banner.appendChild(t)
 }
 
--- chamber/scripts/homejson.js
+++ chamber/scripts/homejson.js
@@ -1,7 +1,6 @@
-
-
 const url =
-"http://127.0.0.1:5501/chamber/data/members.json";
+	"http://127.0.0.1:5501/chamber/data/members.json";
+
 const getMembers = async () => {
 	let members = await jsonFetch(url);
 
@@ -15,28 +14,56 @@ async function jsonFetch(url) {
 	console.log(data)
 	return data.members;
 }
+
+const displayMembers = (members) => {
+	const cards = document.querySelector("#spotlight");
+	cards.innerHTML = "";
+   
+
+	    members.forEach((member) => {
+        
+        
+		let card = document.createElement("section");
+        card.classList.add('sectioncard');
+        let portrait = document.createElement("img");
+        portrait.classList.add('imgem');
+		let p2 = document.createElement("p")
+		let stats = document.createElement("div");
+		stats.classList.add("stats");
+		let date = document.createElement("h3")
+        date.classList.add("dates");
 	
-	const displaypata = (members) => {
+		let place = document.createElement("a");
+        place.classList.add('web');
+		let num = document.createElement("p");
+		num.classList.add("dates");
+
+        place.setAttribute("href",member.website)
+		portrait.setAttribute("src", member.imageurl);
+		portrait.setAttribute(
+			"alt",
+			`${member.name} Company`
+		);
+		portrait.setAttribute("loading", "lazy");
+		portrait.setAttribute("width", "340");
+		portrait.setAttribute("height", "440");
+		p2.textContent = member.spotlight;
+		date.innerHTML = ` ${member.phoneNumbers}`;
+		place.innerHTML = `${member.website}`;
+		num.innerHTML = ` ${member.membershipLvl}`;
+		
+        card.appendChild(portrait);
+		stats.appendChild(date);
+		stats.appendChild(p2)
+		stats.appendChild(place);
+		stats.appendChild(num);
+		card.appendChild(stats);
 		
 
-		const eventname = document.querySelector("#eventspt")
-		const eventspt = document.querySelector("#imagenevent")
-		const eventphone= document.querySelector("#eventphone")
-		const eventweb = document.querySelector("#eventweb")
-		
+		cards.appendChild(card);
+	});
+};
 
+getMembers();
 
-		members.forEach((member) => {
-				
-				eventname.innerHTML = member.name;
-				eventspt.innerHTML = member.spotlight;
-				eventphone.innerHTML = member.phoneNumbers;
-				eventweb.href = members.website;
-				eventweb.innerHTML = member.website;
-				
-
-			})
-		}
-	
-		
-		getMembers();
+// buttons
