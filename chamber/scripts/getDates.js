95bfd06fb357a987c639deb2424beae602277bfd
alexandelton <alexanderelton2887@gmail.com>
2023-02-14 01:06:25 -0500

    newss

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -4,11 +4,7 @@ document.getElementById("lastModified").innerHTML = text;
 
 
 
-var d = new Date();
-var hours = d.getHours();
-var mins = d.getMinutes();
-var seconds = d.getSeconds();
-document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
+
 
 
 
