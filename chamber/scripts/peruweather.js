ad265ca3e290f3d6d40f9bd8fdf89772606ff3db
alexandelton <alexanderelton2887@gmail.com>
2023-02-15 21:10:50 -0500

    newss

--- chamber/scripts/peruweather.js
+++ chamber/scripts/peruweather.js
@@ -6,7 +6,7 @@ const myTemperature = document.querySelector('#current-temp')
 
 
 
-const myUrl = "//api.openweathermap.org/data/2.5/weather?lat=-12.0460&lon=-77.0375&appid=fb48214618cad7e2b789beb97bb86188&q=Lima&units=imperial"
+const myUrl = "https://api.openweathermap.org/data/2.5/weather?lat=-12.0460&lon=-77.0375&appid=fb48214618cad7e2b789beb97bb86188&q=Lima&units=imperial"
 
 
 fetch(myUrl)
@@ -15,7 +15,7 @@ fetch(myUrl)
 
 function displayData(data){
     console.log(data)
-    myGraphic.src = "http://openweathermap.org/img/wn/02d@2x.png"
+    myGraphic.src = "https://openweathermap.org/img/wn/02d@2x.png"
     myGraphic.alt = data.weather[0].main
     myDescription.innerHTML = data.weather[0].description
     myTemperature.innerHTML = data.main.temp+"&deg;F"
--- chamber/scripts/weather3day.js
+++ chamber/scripts/weather3day.js
@@ -13,7 +13,7 @@ const myDescription4 = document.querySelector('#ficaption4')
 const myTemperature4 = document.querySelector('#current-temp4')
 
 
-const myUrl2 = "//api.openweathermap.org/data/2.5/forecast?lat=-12.0432&lon=-77.0282&appid=fb48214618cad7e2b789beb97bb86188&q=Lima&units=imperial"
+const myUrl2 = "https://api.openweathermap.org/data/2.5/forecast?lat=-12.0432&lon=-77.0282&appid=fb48214618cad7e2b789beb97bb86188&q=Lima&units=imperial"
 
 
 
@@ -38,11 +38,11 @@ function displayData(data3){
     myTemperature2.innerHTML = data3.list[i].main.temp+"&deg;F";
     myTemperature3.innerHTML = data3.list[i].main.temp+"&deg;F";
     myTemperature4.innerHTML = data3.list[i].main.temp+"&deg;F";
-    myGraphic1.src = "http://openweathermap.org/img/wn/03n@2x.png";
-    myGraphic2.src = "http://openweathermap.org/img/wn/04n@2x.png";
-    myGraphic3.src = "http://openweathermap.org/img/wn/04n@2x.png";
-    myGraphic4.src = "http://openweathermap.org/img/wn/04d@2x.png";
-    myGraphic3.alt = data3.weather[0].main; }
+    myGraphic1.src = "https://openweathermap.org/img/wn/03n@2x.png";
+    myGraphic2.src = "https://openweathermap.org/img/wn/04n@2x.png";
+    myGraphic3.src = "https://openweathermap.org/img/wn/04n@2x.png";
+    myGraphic4.src = "https://openweathermap.org/img/wn/04d@2x.png";
+     }
    
     
   
