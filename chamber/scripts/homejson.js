9b24bf657bc95ef11499a2a308a1df664f115fef
alexandelton <alexanderelton2887@gmail.com>
2023-02-22 01:58:38 -0500

    newss

--- chamber/scripts/homejson.js
+++ chamber/scripts/homejson.js
@@ -1,5 +1,5 @@
 const url =
-	"http://127.0.0.1:5501/chamber/data/members.json";
+	"https://127.0.0.1:5501/chamber/data/members.json";
 
 const getMembers = async () => {
 	let members = await jsonFetch(url);
